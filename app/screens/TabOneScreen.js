31b931f1513f9df81daced6f0d078df3f53788dc
shide <shide.adibi@gmail.com>
2022-02-16 20:43:02 +0100

    fixed the styles

--- app/screens/TabOneScreen.js
+++ app/screens/TabOneScreen.js
@@ -7,8 +7,8 @@ import ItemBox from "../components/ItemBox";
 export default function TabOneScreen({navigation}) {
     return (
         <View style={styles.container}>
-            <ScrollView style={styles.itemBoxContainer}
-                        showsVerticalScrollIndicator={false} contentContainerStyle={styles.itemBoxContainer}>
+            <ScrollView showsVerticalScrollIndicator={false}
+                        contentContainerStyle={styles.itemBoxContainer}>
                 <ItemBox style={styles.itemBox}></ItemBox>
                 <ItemBox style={styles.itemBox}></ItemBox>
                 <ItemBox style={styles.itemBox}></ItemBox>
@@ -36,11 +36,13 @@ const styles = StyleSheet.create({
     },
     itemBoxContainer: {
         flexWrap:'wrap',
-        flexDirection: 'row'
+        flexDirection: 'row',
+        justifyContent:'center',
+        margin: 10
     },
     itemBox: {
         flexBasis: '45%',
-        margin: 2,
+        margin: 3,
         backgroundColor: 'orange',
     }
 });
